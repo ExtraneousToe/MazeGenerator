4ee6071362ac8b29013595279108d5386b940048
MattCameronLVR <61762139+MattCameronLVR@users.noreply.github.com>
2020-09-14 15:57:06 +1000

    Setting up code

---
+++ Project/Assets/Scripts/MazeGeneration/PathAlgorithm.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MazeGeneration
+{
+    public abstract class PathAlgorithm : ScriptableObject
+    {
+        public abstract void GeneratePath();
+    } 
+}
