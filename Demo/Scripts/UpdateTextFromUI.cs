466332f9db41b2d6af84558e375054dfedfbae99
MattCameronLVR <61762139+MattCameronLVR@users.noreply.github.com>
2020-09-16 17:03:55 +1000

    Added TMPro stuff, setting up a demo scene for uploading to GH Pages

---
+++ Demo/Scripts/UpdateTextFromUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace MazeGeneration.Demo
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class UpdateTextFromUI : MonoBehaviour
+    {
+        #region Variables
+        [SerializeField] private TMP_Text m_text;
+        #endregion
+
+        #region Mono
+        protected void Reset()
+        {
+            m_text = GetComponent<TMP_Text>();
+        }
+        #endregion
+
+        #region Mutators
+        public void SetText(int value)
+        {
+            m_text?.SetText($"{value}");
+        }
+
+        public void SetText(float value)
+        {
+            m_text?.SetText($"{value}");
+        }
+        #endregion
+    }
+}
